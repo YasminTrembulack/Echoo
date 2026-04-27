04a06cb96b9c4d3bbe632fdbd34260e8c1123e11
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-04-27 17:09:43 -0300

    feat: create UserRepository with unique field checks

--- src/main/java/com/trycatchus/echoo/repository/UserRepository.java
+++ src/main/java/com/trycatchus/echoo/repository/UserRepository.java
@@ -1,12 +1,22 @@
 package com.trycatchus.echoo.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.trycatchus.echoo.model.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
+
+    @Query("""
+    SELECT u FROM User u
+    WHERE u.email = :email
+       OR u.cpf = :cpf
+       OR u.username = :username
+    """)
+    List<User> findConflictingUsers(String email, String cpf, String username);
 }
