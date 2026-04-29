b17016c12c8cbd5c35aca3e0ab95de632c671164
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-04-29 10:26:08 -0300

    feat: update interface for new auth methods

--- src/main/java/com/trycatchus/echoo/interfaces/AuthService.java
+++ src/main/java/com/trycatchus/echoo/interfaces/AuthService.java
@@ -1,12 +1,10 @@
 package com.trycatchus.echoo.interfaces;
 
-import com.auth0.jwt.interfaces.DecodedJWT;
-
-import com.trycatchus.echoo.dto.payload.login.LoginPayload;
-import com.trycatchus.echoo.dto.responses.login.AuthResponse;
+import com.trycatchus.echoo.dto.payload.auth.LoginPayload;
+import com.trycatchus.echoo.dto.responses.UserResponse;
+import com.trycatchus.echoo.dto.responses.auth.AuthResponse;
 
 public interface AuthService {
-    AuthResponse loginAsync(LoginPayload payload);
-
-    DecodedJWT decodeTokenAsync(String token);
+    AuthResponse login(LoginPayload payload);
+    UserResponse verifyToken(String token);
 }
