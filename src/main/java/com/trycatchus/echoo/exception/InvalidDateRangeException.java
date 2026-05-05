0fe16d97f89d3bc7f03bd8206a5f915cb528c165
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-05-05 14:21:37 -0300

    chore: update exc message

--- src/main/java/com/trycatchus/echoo/exception/InvalidDateRangeException.java
+++ src/main/java/com/trycatchus/echoo/exception/InvalidDateRangeException.java
@@ -2,7 +2,7 @@ package com.trycatchus.echoo.exception;
 
 public class InvalidDateRangeException extends ApplicationException {
     public InvalidDateRangeException() {
-        super(400, "Invalid date range.");
+        super(400, "Invalid date range: startDate must be before endDate.");
     }
     public InvalidDateRangeException(String message) {
         super(400, message);
