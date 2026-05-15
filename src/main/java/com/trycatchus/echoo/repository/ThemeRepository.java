8e1a633367c2b52ec314c4ff79e46ed7d96f6a0e
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-05-15 10:04:49 -0300

    feat: add new methods to theme repository

--- src/main/java/com/trycatchus/echoo/repository/ThemeRepository.java
+++ src/main/java/com/trycatchus/echoo/repository/ThemeRepository.java
@@ -3,9 +3,20 @@ package com.trycatchus.echoo.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.trycatchus.echoo.model.Theme;
 
 public interface ThemeRepository extends JpaRepository<Theme, UUID> {
+    @Query("""
+        SELECT CASE WHEN COUNT(t) > 0 THEN true ELSE false END
+        FROM Theme t
+        WHERE t.name = :name
+        AND (:themeIdToExclude IS NULL OR t.id <> :themeIdToExclude)
+    """)
+    Boolean existsConflictingTheme(
+        String name,
+        UUID themeIdToExclude
+    );
 
 }
