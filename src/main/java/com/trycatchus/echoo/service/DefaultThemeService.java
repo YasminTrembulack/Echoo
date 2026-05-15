e1e141d66b91ec4dd164afcd194d706e7d062798
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-05-15 10:07:38 -0300

    feat: create theme service impl and theme controller

---
+++ src/main/java/com/trycatchus/echoo/service/DefaultThemeService.java
@@ -0,0 +1,90 @@
+package com.trycatchus.echoo.service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.trycatchus.echoo.dto.payload.theme.ThemePayload;
+import com.trycatchus.echoo.dto.payload.theme.ThemeUpdatePayload;
+import com.trycatchus.echoo.dto.responses.ThemeResponse;
+import com.trycatchus.echoo.exception.EntityNotFoundException;
+import com.trycatchus.echoo.exception.UniqueFieldAlreadyInUseException;
+import com.trycatchus.echoo.interfaces.ThemeService;
+import com.trycatchus.echoo.mapper.ThemeMapper;
+import com.trycatchus.echoo.model.Theme;
+import com.trycatchus.echoo.repository.ThemeRepository;
+import com.trycatchus.echoo.utils.UpdateUtils;
+
+@Service
+public class DefaultThemeService implements ThemeService{
+
+    private final ThemeRepository themeRepo;
+    private final ThemeMapper themeMapper;
+
+    public DefaultThemeService(ThemeRepository themeRepo, ThemeMapper themeMapper) {
+        this.themeRepo = themeRepo;
+        this.themeMapper = themeMapper;
+    }
+
+    private void validateUniqueFields(String name, UUID themeIdToExclude) {
+        Boolean exists = themeRepo.existsConflictingTheme(name, themeIdToExclude);
+
+        if (exists)
+            throw new UniqueFieldAlreadyInUseException(Theme.class, List.of("name"));
+    }   
+
+    @Override
+    public ThemeResponse create(ThemePayload payload) {
+        validateUniqueFields(payload.name(), null);
+
+        Theme theme = themeMapper.toEntity(payload);
+
+        Theme savedTheme = themeRepo.save(theme);
+
+        return themeMapper.toResponse(savedTheme);
+    }
+
+    @Override
+    public ThemeResponse update(String id, ThemeUpdatePayload payload) {
+        Theme theme = themeRepo.findById(UUID.fromString(id))
+            .orElseThrow(() -> new EntityNotFoundException(Theme.class));
+        
+        theme.setName(UpdateUtils.valueOrKeep(payload.name(), theme.getName()));
+
+        validateUniqueFields(theme.getName(), UUID.fromString(id));
+
+        theme.setDescription(UpdateUtils.valueOrKeep(payload.descriprion(), theme.getDescription()));
+
+        Theme updatedTheme = themeRepo.save(theme);
+
+        return themeMapper.toResponse(updatedTheme);
+    }
+
+    @Override
+    public void delete(String id) {
+        Theme theme = themeRepo.findById(UUID.fromString(id))
+            .orElseThrow(() -> new EntityNotFoundException(Theme.class));
+        
+        themeRepo.delete(theme);
+    }
+
+    @Override
+    public ThemeResponse findById(String id) {
+        Theme theme = themeRepo.findById(UUID.fromString(id))
+            .orElseThrow(() -> new EntityNotFoundException(Theme.class));
+        
+        return themeMapper.toResponse(theme);
+    }
+
+    @Override
+    public List<ThemeResponse> findAll() {
+        List<Theme> themes = themeRepo.findAll();
+
+        return themes.stream()
+            .map(themeMapper::toResponse)
+            .collect(Collectors.toList());
+    }
+    
+}
